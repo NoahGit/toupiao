6950da3318626963f4b8482bbd4cdea359a685fe
noah <gaoyong@xatu.edu.cn>
2019-08-29 16:25:43 +0800

    Initial commit

---
+++ polls/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+# Create your models here.
+# 在这个简单的投票应用中，需要创建两个模型：问题 Question 和选项 Choice。
+# Question 模型包括问题描述和发布时间。
+# Choice 模型有两个字段，选项描述和当前得票数。每个选项属于一个问题。
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    # 这是个演示方法，调用这个方法为了判断某个问题是不是最近的时间发表的
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now()-datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # 我们使用 ForeignKey 定义了一个关系。这将告诉 Django，每个 Choice 对象都关联到一个 Question 对象。
+    # Django 支持所有常用的数据库关系：多对一、多对多和一对一。
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
---
+++ polls/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
+
+    # ex: /pools/
+    # path('', views.index, name='index'),
+    # ex: /pools/5/
+    # 问题详情页——展示某个投票的问题和不带结果的选项列表。
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    # 问题结果页——展示某个投票的结果。
+    # path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    # 投票处理器——用于响应用户为某个问题的特定选项投票的操作。
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+
+    # path有四个参数
+    # （route匹配url的准则，类似正则表达式，例如www.b.com/myApp/?page=3中，只会匹配myApp/
+    # view当找到了匹配的准则，就会调用这个特定的视图函数，并传入一个HttpResponse对象作为第一个参数，被“捕获”的参数以关键字参数的形式传入。
+    # kwargs任意一个关键字参数可以作为一个字典传递给目标视图函数。
+    # name为你的url取名能使你在Django的任意地方唯一的引用它，尤其是在模版中。这个特性允许你只改一个文件就能全局地修改某个url模式。）
+]
